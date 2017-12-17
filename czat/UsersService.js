888a62877c2fd21eaa977bc696f632dedf3c8c4f
Magdalena Michalska <magda@MacBook-Pro-Magdalena.local>
2017-12-17 09:29:04 +0100

    first commit

---
+++ czat/UsersService.js
@@ -0,0 +1,29 @@
+class UsersService {
+    constructor() {
+      this.users = [];
+    }
+  
+    getAllUsers() {
+      return this.users;
+    }
+  
+    getUserById(userId) {
+      return this.users.find(user => user.id === userId);
+    }
+  
+    addUser(user) {
+      this.users = [user, ...this.users];
+    }
+  
+    removeUser(userId) {
+      this.users = this.users.filter(user => user.id !== userId);
+    }
+
+    printusers() {
+      this.users.map((user) => {
+          console.log(user);
+      });
+    }
+}
+
+module.exports = UsersService;
---
+++ czat/client/App.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import io from 'socket.io-client';
+import styles from './App.css';
+
+import MessageForm from './MessageForm';
+import MessageList from './MessageList';
+import UsersList from './UsersList';
+import UserForm from './UserForm';
+
+const socket = io('http://localhost:3000');
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { users: [], messages: [], text: '', name:'' };
+    }
+    componentDidMount() {
+        socket.on('message', message => this.messageReceive(message));
+        socket.on('update', ({users}) => this.chatUpdate(users));
+    }
+    messageReceive(message) {
+        const messages = [message, ...this.state.messages];
+        this.setState({ messages });
+    }
+    chatUpdate(users) {
+        this.setState({users});
+    }
+    handleMessageSubmit(message) {
+        const messages = [message, ...this.state.messages];
+        this.setState({ messages });
+        socket.emit('message', message);
+    }
+    handleUserSubmit(name) {
+        this.setState({name});
+        socket.emit('join', name);
+    }
+    render() {
+        return this.state.name !== '' ? this.renderLayout() : this.renderUserForm();
+    }
+    renderLayout() {
+        return (
+            <div className={styles.App}>
+                <div className={styles.AppHeader}>
+                    <div className={styles.AppTitle}>
+                        ChatApp
+                    </div>
+                    <div className={styles.AppRoom}>
+                        AppRoom
+                    </div>
+                </div>
+                <div className={styles.AppBody}>
+                    <div className={styles.MessageWrapper}>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+    renderUserForm() {
+        return (<UserForm onUserSubmit= { name => this.handleUserSubmit(name) }/>);
+    }
+};
+
+export default App;
